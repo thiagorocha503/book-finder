0d447a9c08020108e76a18bd98cd927b11a846da
Thiago <thiagorocha503@gmail.com>
2022-11-26 19:30:45 -0300

    ✨ Add pagination

--- js/script.js
+++ js/script.js
@@ -1,66 +1,114 @@
 "use strict";
-var GOOGLE_API_VERSION = "1";
+var API_VERSION = "1";
+var BASE_API_URL = "https://www.googleapis.com";
 var inputSearch = document.getElementById("txt-search");
-var ENTER_KEY = 13;
-inputSearch.addEventListener("keypress", function (evt) {
-    if (evt.keyCode == ENTER_KEY) {
-        onSearch();
+var ENTER_KEY = "Enter";
+// pagination
+var total = 0;
+var limit = 20;
+var offset = 0;
+var currentQuery = "";
+var scrollBlock = false;
+// events
+window.addEventListener("scroll", function () {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight - 50) {
+        if (!scrollBlock) {
+            offset += limit;
+            scrollBlock = true;
+            search(currentQuery, offset);
+        }
     }
 });
-function search(query) {
-    var xmlHtmlResquest = new XMLHttpRequest();
-    xmlHtmlResquest.onreadystatechange = function (evt) {
-        if (xmlHtmlResquest.readyState == 4) {
+inputSearch.addEventListener("keypress", function (evt) {
+    if (evt.code == ENTER_KEY) {
+        inputSearch.classList.remove("is-invalid");
+        var feedBack = document.getElementById("search-feedback");
+        feedBack === null || feedBack === void 0 ? void 0 : feedBack.classList.remove("d-block");
+        if (inputSearch.value == "") {
+            console.warn("Empty query");
+            inputSearch.classList.add("is-invalid");
+            feedBack === null || feedBack === void 0 ? void 0 : feedBack.classList.add("d-block");
+            return;
+        }
+        search(inputSearch.value);
+    }
+});
+function onSearch() {
+    search(inputSearch.value);
+}
+function search(query, offset) {
+    if (offset === void 0) { offset = 0; }
+    // end result
+    if (offset != 0 && (offset + limit) > total) {
+        return;
+    }
+    var url = "".concat(BASE_API_URL, "/books/v").concat(API_VERSION, "/volumes?q=").concat(query, "&startIndex=").concat(offset, "&maxResults=").concat(limit);
+    console.info(url);
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("get", url);
+    xhttp.onloadstart = function () {
+        if (offset == 0) {
+            $("#loading").css("display", "block");
+            $("#card-list").html("");
+        }
+        else {
+            $("#loading").css("display", "none");
+            // loading bottom
+            var loading_bottom = document.createElement("div");
+            loading_bottom.id = "loading-bottom";
+            loading_bottom.style.padding = "8px";
+            loading_bottom.classList.add("text-center");
+            var img = document.createElement("img");
+            img.src = "img/loading.gif";
+            loading_bottom.append(img);
+            $("#card-list").append(loading_bottom);
+        }
+    };
+    xhttp.onreadystatechange = function (evt) {
+        if (xhttp.readyState == 4) {
             console.log("Read state 4: DONE");
-            if (xmlHtmlResquest.status == 200) {
-                var reponseJSON = JSON.parse(xmlHtmlResquest.responseText);
+            if (xhttp.status == 200) {
+                var reponseJSON = JSON.parse(xhttp.responseText);
                 var books = reponseJSON["totalItems"] != 0 ? reponseJSON["items"] : [];
-                render(books);
+                total = reponseJSON["totalItems"];
+                var container = document.getElementById("row-result");
+                var oldCardList = document.getElementById("card-list");
                 $("#loading").css("display", "none");
-            }
-            else {
-                console.log("status: " + xmlHtmlResquest.status);
-                $("#loading").css("display", "none");
+                $("#loading-bottom").remove();
+                if (books.length == 0) {
+                    var emptyList = document.createElement("div");
+                    emptyList.id = "card-list";
+                    emptyList.className = "col mb-4 text-muted";
+                    emptyList.innerHTML = "No results found";
+                    container.replaceChild(emptyList, oldCardList);
+                }
+                else {
+                    if (offset == 0) {
+                        container.replaceChild(buildListCard(books), oldCardList);
+                    }
+                    else {
+                        var list = document.getElementById("card-list");
+                        for (var i = 0; i < books.length; i++) {
+                            var card = buildCard(books[i]);
+                            list === null || list === void 0 ? void 0 : list.appendChild(card);
+                        }
+                    }
+                }
+                currentQuery = query;
+                scrollBlock = false;
             }
         }
     };
-    xmlHtmlResquest.timeout = 3000;
-    xmlHtmlResquest.onerror = function (evt) {
-        console.error("on Error> " + xmlHtmlResquest.status);
+    xhttp.onerror = function (evt) {
+        console.error("Status code: " + xhttp.status);
+        if (xhttp.status == 404) {
+            $("#modal-erro-body").text("Not found resource");
+        }
         $("#error-modal").modal("show");
         $("#loading").css("display", "none");
     };
-    xmlHtmlResquest.open("get", "https://www.googleapis.com/books/v" + GOOGLE_API_VERSION + "/volumes?q=" + query);
-    xmlHtmlResquest.send();
-    $("#card-list").html("");
-    $("#loading").css("display", "block");
-}
-function onSearch() {
-    inputSearch.classList.remove("is-invalid");
-    var feedBack = document.getElementById("search-feedback");
-    feedBack === null || feedBack === void 0 ? void 0 : feedBack.classList.remove("d-block");
-    if (inputSearch.value == "") {
-        console.log("Empty query");
-        inputSearch.classList.add("is-invalid");
-        feedBack === null || feedBack === void 0 ? void 0 : feedBack.classList.add("d-block");
-        return;
-    }
-    search(inputSearch.value);
-}
-function render(books) {
-    var container = document.getElementById("row-result");
-    var oldCardList = document.getElementById("card-list");
-    var newCardList;
-    if (books.length == 0) {
-        newCardList = document.createElement("div");
-        newCardList.id = "card-list";
-        newCardList.className = "col mb-4 text-muted";
-        newCardList.innerHTML = "No results found";
-    }
-    else {
-        newCardList = buildListCard(books);
-    }
-    container.replaceChild(newCardList, oldCardList);
+    xhttp.timeout = 3000;
+    xhttp.send();
 }
 function buildListCard(books) {
     var cardList = document.createElement("div");
@@ -73,11 +121,11 @@ function buildListCard(books) {
     return cardList;
 }
 function buildCard(book) {
-    console.log(book);
+    // console.log(book);
     var cardContainer = document.createElement("div");
     cardContainer.className = "card shadow-sm mb-2";
-    cardContainer.insertAdjacentHTML("afterbegin", "\n        <div class=\"card-header d-block d-sm-none\">\n            <h5 class=\"text-gray\">" + book["volumeInfo"]["title"] + "</h5>\n        </div>\n        <div class=\"card-body d-flex\">\n            <div style=\"min-width: 128px;\">\n                <img class=\"fluid-img\" alt=\"image not available\" src=" + (book["volumeInfo"]["imageLinks"] != undefined ? book["volumeInfo"]["imageLinks"]["smallThumbnail"] : "img/thumbnail.png") + ">\n            </div>\n            <div class=\"px-3 flex-grow-1\">\n                <h4 class=\"text-gray d-none d-sm-block\">" + book["volumeInfo"]["title"] + "</h4>\n                <div class=\"d-flex flex-column\" >\n                    <p class=\"order-sm-2\">\n                        " + (book["searchInfo"] != undefined ? book["searchInfo"]["textSnippet"] : "") + "\n                    </p>\n                    <p class=\"order-sm-1 text-gray text-muted\">\n                        " + (book["volumeInfo"]["authors"] != undefined ?
-        book["volumeInfo"]["authors"].map(function (item, i) { return item.trim(); }).join("") : "author unknown") + "\n                        " + (book["volumeInfo"]["publishedDate"] != undefined ? " | " + formatDate(book["volumeInfo"]["publishedDate"]) : "") + "\n                    </p>\n                </div>\n                <a href=" + (book["volumeInfo"]["infoLink"] != undefined ? book["volumeInfo"]["infoLink"] : "") + " class=\"btn btn-primary btn-sm text-white\" target=\"_blank\" >See more</a>\n            </div>\n        </div>\n    ");
+    cardContainer.insertAdjacentHTML("afterbegin", "\n        <div class=\"card-header d-block d-sm-none\">\n            <h5 class=\"text-gray\">".concat(book["volumeInfo"]["title"], "</h5>\n        </div>\n        <div class=\"card-body d-flex\">\n            <div style=\"min-width: 128px;\">\n                <img  class=\"fluid-img\" alt=\"image not available\" src=").concat(book["volumeInfo"]["imageLinks"] != undefined ? book["volumeInfo"]["imageLinks"]["smallThumbnail"] : "img/thumbnail.png", ">\n            </div>\n            <div class=\"px-3 flex-grow-1\">\n                <h4 class=\"text-gray d-none d-sm-block\">").concat(book["volumeInfo"]["title"], "</h4>\n                <div class=\"d-flex flex-column\" >\n                    <p class=\"order-sm-2\">\n                        ").concat(book["searchInfo"] != undefined ? book["searchInfo"]["textSnippet"] : "", "\n                    </p>\n                    <p class=\"order-sm-1 text-gray text-muted\">\n                        ").concat(book["volumeInfo"]["authors"] != undefined ?
+        book["volumeInfo"]["authors"].map(function (item, i) { return item.trim(); }).join("") : "author unknown", "\n                        ").concat(book["volumeInfo"]["publishedDate"] != undefined ? " | " + formatDate(book["volumeInfo"]["publishedDate"]) : "", "\n                    </p>\n                </div>\n                <a href=").concat(book["volumeInfo"]["infoLink"] != undefined ? book["volumeInfo"]["infoLink"] : "", " class=\"btn btn-primary btn-sm text-white\" target=\"_blank\" >See more</a>\n            </div>\n        </div>\n    "));
     return cardContainer;
 }
 function formatDate(date) {
--- js/script.ts
+++ js/script.ts
@@ -1,71 +1,121 @@
-const GOOGLE_API_VERSION = "1";
+const API_VERSION = "1";
+const BASE_API_URL = "https://www.googleapis.com";
 var inputSearch: HTMLInputElement = document.getElementById("txt-search") as HTMLInputElement;
-const ENTER_KEY: number = 13
+const ENTER_KEY: string = "Enter";
 
-inputSearch.addEventListener("keypress", (evt: KeyboardEvent) => {
-    if (evt.keyCode == ENTER_KEY) {
-        onSearch();
+// pagination
+let total: number = 0;
+let limit: number = 20;
+let offset = 0;
+let currentQuery = "";
+
+let scrollBlock: boolean= false;
+
+// events
+window.addEventListener("scroll" ,()=>{
+    if ((window.innerHeight + window.pageYOffset)>= document.body.offsetHeight-50) {
+        if(!scrollBlock){
+            offset += limit; 
+            scrollBlock=true;
+            search(currentQuery, offset);
+        }
     }
 });
 
-function search(query: string) {
-    let xmlHtmlResquest: XMLHttpRequest = new XMLHttpRequest();
-    xmlHtmlResquest.onreadystatechange = (evt: Event) => {
-        if (xmlHtmlResquest.readyState == 4) {
+
+inputSearch.addEventListener("keypress", (evt: KeyboardEvent) => {
+    if (evt.code == ENTER_KEY) {
+        inputSearch.classList.remove("is-invalid");
+        let feedBack = document.getElementById("search-feedback");
+        feedBack?.classList.remove("d-block")
+        if (inputSearch.value == "") {
+            console.warn("Empty query");
+            inputSearch.classList.add("is-invalid");
+            feedBack?.classList.add("d-block");
+            return;
+        }
+        search(inputSearch.value);
+    }
+});
+
+
+function onSearch(){
+    search(inputSearch.value)
+}
+
+function search(query: string, offset:number=0) {
+    // end result
+    if( offset != 0 && (offset + limit)>total){
+        return;
+    }
+    let url = `${BASE_API_URL}/books/v${API_VERSION}/volumes?q=${query}&startIndex=${offset}&maxResults=${limit}`;
+    console.info(url);
+
+    let xhttp: XMLHttpRequest = new XMLHttpRequest();
+    xhttp.open("get", url) 
+    xhttp.onloadstart = ()=> {
+        if(offset == 0){
+            $("#loading").css("display","block");
+            $("#card-list").html("");
+        }else{
+            $("#loading").css("display","none");          
+            // loading bottom
+            let loading_bottom = document.createElement("div") as HTMLDivElement;
+            loading_bottom.id = "loading-bottom";
+            loading_bottom.style.padding = "8px"
+            loading_bottom.classList.add("text-center")
+            let img = document.createElement("img") as HTMLImageElement;
+            img.src = "img/loading.gif";
+            loading_bottom.append(img)
+            $("#card-list").append(loading_bottom);
+        }
+
+    }
+    xhttp.onreadystatechange = (evt: Event) => {
+        if (xhttp.readyState == 4) {        
             console.log("Read state 4: DONE")
-            if (xmlHtmlResquest.status == 200) {
-                let reponseJSON = JSON.parse(xmlHtmlResquest.responseText);
+            if (xhttp.status == 200) {            
+                let reponseJSON = JSON.parse(xhttp.responseText);
                 let books = reponseJSON["totalItems"] != 0 ? reponseJSON["items"] : [];
-                render(books);
+                total = reponseJSON["totalItems"];     
+                let container: HTMLDivElement = document.getElementById("row-result") as HTMLDivElement;
+                let oldCardList: HTMLDivElement = document.getElementById("card-list") as HTMLDivElement;
                 $("#loading").css("display","none");
-            } else {
-                console.log("status: " + xmlHtmlResquest.status)
-                $("#loading").css("display","none");
-            }
+                $("#loading-bottom").remove();
+                if(books.length == 0){
+                    let emptyList: HTMLDivElement = document.createElement("div");
+                    emptyList.id = "card-list";
+                    emptyList.className = "col mb-4 text-muted";
+                    emptyList.innerHTML = "No results found";
+                    container.replaceChild( emptyList, oldCardList);
+                }else{
+                    if(offset == 0) {                
+                        container.replaceChild(buildListCard(books),oldCardList);             
+                    }else{                     
+                        let list = document.getElementById("card-list");
+                        for( let i:number=0; i<books.length;i++){
+                            let card = buildCard(books[i]);
+                            list?.appendChild(card);
+                        }   
+                    }                                     
+                }              
+                currentQuery = query;
+                scrollBlock=false            
+            }         
         }
     }
-    xmlHtmlResquest.timeout = 3000;
-    xmlHtmlResquest.onerror = function (evt) {
-        console.error("on Error> " + xmlHtmlResquest.status)
+    xhttp.onerror = function (evt) {
+        console.error("Status code: " + xhttp.status);
+        if(xhttp.status ==404){
+            $("#modal-erro-body").text("Not found resource")
+        }
         $("#error-modal").modal("show");
         $("#loading").css("display","none");
     }
-    xmlHtmlResquest.open("get", `https://www.googleapis.com/books/v${GOOGLE_API_VERSION}/volumes?q=${query}`)
-    xmlHtmlResquest.send();
-    $("#card-list").html("");
-    $("#loading").css("display","block");
+    xhttp.timeout = 3000;
+    xhttp.send(); 
 }
-
-function onSearch() {
-    inputSearch.classList.remove("is-invalid");
-    let feedBack = document.getElementById("search-feedback");
-    feedBack?.classList.remove("d-block")
-    if (inputSearch.value == "") {
-        console.log("Empty query");
-        inputSearch.classList.add("is-invalid");
-        feedBack?.classList.add("d-block");
-        return;
-    }
-    search(inputSearch.value);
-}
-
-function render(books: Array<Object>) {
-    let container: HTMLDivElement = document.getElementById("row-result") as HTMLDivElement;
-    let oldCardList: HTMLDivElement = document.getElementById("card-list") as HTMLDivElement;
-    let newCardList: HTMLDivElement;
-    if (books.length == 0) {
-        newCardList = document.createElement("div");
-        newCardList.id = "card-list";
-        newCardList.className = "col mb-4 text-muted";
-        newCardList.innerHTML = "No results found";
-    } else {
-        newCardList = buildListCard(books);
-    }
-    container.replaceChild(newCardList, oldCardList);
-
-}
-
-function buildListCard(books: Array<any>) {
+function buildListCard(books: Array<any>) { 
     let cardList: HTMLDivElement = document.createElement("div");
     cardList.className = "col mb-4";
     cardList.id = "card-list";
@@ -77,7 +127,7 @@ function buildListCard(books: Array<any>) {
 }
 
 function buildCard(book: any): HTMLDivElement {
-    console.log(book);
+    // console.log(book);
     let cardContainer: HTMLDivElement = document.createElement("div");
     cardContainer.className = "card shadow-sm mb-2";
     cardContainer.insertAdjacentHTML("afterbegin", `
@@ -86,7 +136,7 @@ function buildCard(book: any): HTMLDivElement {
         </div>
         <div class="card-body d-flex">
             <div style="min-width: 128px;">
-                <img class="fluid-img" alt="image not available" src=${book["volumeInfo"]["imageLinks"] != undefined ? book["volumeInfo"]["imageLinks"]["smallThumbnail"] : "img/thumbnail.png"}>
+                <img  class="fluid-img" alt="image not available" src=${book["volumeInfo"]["imageLinks"] != undefined ? book["volumeInfo"]["imageLinks"]["smallThumbnail"] : "img/thumbnail.png"}>
             </div>
             <div class="px-3 flex-grow-1">
                 <h4 class="text-gray d-none d-sm-block">${book["volumeInfo"]["title"]}</h4>
